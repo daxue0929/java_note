506029925d8d4c90acd80024ce1377c58e387249
daxue0929 <xuediwang@yufuid.com>
2023-04-14 17:25:59 +0800

    Initial commit

---
+++ src/test/Start.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.HashMap;
+
+/**
+ * @author daxue0929
+ * @date 2023/4/14
+ */
+
+public class Start {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1", "213");
+        map.put("65r", "das");
+    }
+}
